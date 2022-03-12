4ae3aae309e74d9710cc940d6f008e5a437633aa
Sharandeep Singh <sharandeepsingh.003@gmail.com>
2022-03-12 20:36:36 +0530

    Login and Signup

--- Mulya-app/src/Components/Login/Login.jsx
+++ Mulya-app/src/Components/Login/Login.jsx
@@ -4,7 +4,21 @@ import './Login.css'
 export default function Login() {
   return (
     <div>
-        <h1 className="brand-heading">MULYA</h1>
+    <h1 className="brand-heading">MULYA</h1>    
+        <form>
+          <div className="form-group">
+              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+          </div>
+          <div className="form-group">
+              <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+          </div>
+          <div className="form-group form-check">
+            <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+              <label className="form-check-label" for="exampleCheck1">Check me out</label>
+          </div>
+          <button type="submit" className="btn btn-primary">Login</button>
+          <button type="submit" className="btn btn-primary">Sign Up</button>
+</form>
     </div>
   )
 }
